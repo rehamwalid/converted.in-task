31eb998361a3fef91de8c65b8275f3196608ccb0
rehamwalid <rehammourad9@gmail.com>
2023-07-11 11:36:02 +0300

    Added tests

---
+++ database/factories/statsFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class statsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory()->create(),
+            'tasks_no' => fake()->numberBetween(1,10)
+        ];
+    }
+
+}
--- tests/Unit/TaskControllerTest.php
+++ tests/Unit/TaskControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Models\Admin;
+use App\Models\Statistics;
 use App\Models\Task;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -27,5 +28,36 @@ class TaskControllerTest extends TestCase
 
         $response=$this->post('/tasks',$request);
         $this->assertDatabaseHas('tasks', ["title" => "What"]);
+    }
+
+    public function stats_update_test()
+    {
+        $userId= User::factory()->create()->id;
+
+        $stat=Statistics::factory()->create([
+            'user_id' => $userId,
+        ]);
+        $oldTaskNumber=$stat->tasks_no;
+        $request=[
+            "title" => "What",
+            "description" => "Hello There",
+            "assigned_to_id" => $userId,
+            "assigned_by_id" => Admin::factory()->create(),
+        ];
+
+        $response=$this->post('/tasks',$request);
+        $this->assertDatabaseHas('statistics', ["user_id" =>$userId,'tasks_no' => $oldTaskNumber+1]);
+    }
+
+    public function list_tasks()
+    {
+        $this->expectException(ValidationException::class);
+        $request=[
+            "title" => "What",
+            "description" => "Hello There",
+            "assigned_to_id" => null,
+            "assigned_by_id" => Admin::factory()->create(),
+        ];
+        $response=$this->post('/tasks',$request);
     }
 }
